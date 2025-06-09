2478a635bc4aac06aa251502e254fdf9c8ede9dd
Carlos Miguel Anchayhua Ramos <carlos.anchayhua@delfosti.com>
2025-06-09 12:26:34 -0500

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Carousel from "./components/carousel";
 import Sobre from "./components/sobre";
 import Products from "./components/products";
 import Footer from "./components/footer";
-import { useRef } from "react";
 
 function App() {
   return (
